d908327b06e8f4946d862560162a55eb6d124171
TranLinh <tranlinhhqb@gmail.com>
2021-07-28 23:33:37 +0700

    update

--- src/components/packages/dialogUpdatePackage.js
+++ src/components/packages/dialogUpdatePackage.js
@@ -1,10 +1,10 @@
 import {
- Autocomplete,
- DialogActions,
- DialogContent,
- DialogTitle,
- Grid,
- TextField
+    Autocomplete,
+    DialogActions,
+    DialogContent,
+    DialogTitle,
+    Grid,
+    TextField
 } from '@material-ui/core';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
@@ -12,164 +12,164 @@ import { Form, Formik } from 'formik';
 import React, { memo, useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import {
- createPackage,
- PackageID,
- updatePackage
+    createPackage,
+    PackageID,
+    updatePackage
 } from 'src/actions/packageAction';
 import { DisplayingErrorMessagesPackageSchema } from 'src/services/ValidConstants';
 import { listAllService } from './../../actions/serviceAction';
 
 const DialogUpdatePackage = ({ data, open, onClose }) => {
- const [totalPrice, setTotalPrice] = useState(0);
+    const [totalPrice, setTotalPrice] = useState(data.price);
 
- const { services } = useSelector((state) => state.serviceListAll);
+    const { services } = useSelector((state) => state.serviceListAll);
 
- const triggerReload = useSelector((state) => state.triggerReload);
- const dispatch = useDispatch();
-
- useEffect(() => {
-  dispatch(listAllService());
- }, [dispatch, triggerReload]);
-
- const submitHandler = ({ name, description, price, services }) => {
-  const mappedData = {
-   name,
-   description,
-   price,
-   serviceIdList: services.map((service) => service.id)
-  };
-  console.log(mappedData, 'debug');
-  //   dispatch(updatePackage(mappedData));
-  onClose();
- };
-
- const handleReset = () => {};
-
- const setCalculatedTotal = (services) => {
-  const total = services.reduce((accumulator, currentValue) => {
-   return (accumulator += currentValue.price);
-  }, 0);
-
-  setTotalPrice(total - total * 0.1);
- };
-
- return (
-  <Formik
-   initialValues={{
-    name: '',
-    description: '',
-    services: []
-   }}
-   validationSchema={DisplayingErrorMessagesPackageSchema}
-   validateOnChange
-   validateOnBlur
-   onSubmit={submitHandler}
-   onReset={handleReset}
-  >
-   {({ errors, handleBlur, handleChange, values, setFieldValue }) => {
+    const triggerReload = useSelector((state) => state.triggerReload);
+    const dispatch = useDispatch();
     useEffect(() => {
-     setFieldValue('price', totalPrice);
-    }, [totalPrice, setFieldValue]);
+        dispatch(listAllService());
+    }, [dispatch, triggerReload]);
+
+    const submitHandler = ({ name, description, price, services }) => {
+        const mappedData = {
+            name,
+            description,
+            price,
+            serviceIdList: services.map((service) => service.id)
+        };
+        console.log(mappedData, 'debug');
+        dispatch(updatePackage(mappedData));
+        onClose();
+    };
+
+    const handleReset = () => { };
+
+    const setCalculatedTotal = (services) => {
+        const total = services.reduce((accumulator, currentValue) => {
+            return (accumulator += currentValue.price);
+        }, 0);
+
+        setTotalPrice(total - total * 0.1);
+    };
 
     return (
-     <Dialog
-      onClose={onClose}
-      aria-describedby="scroll-dialog-description"
-      open={open}
-      maxWidth={'xs'}
-      fullWidth={true}
-     >
-      <Form>
-       <DialogTitle id="customized-dialog-title" onClose={onClose}>
-        Gói Dịch vụ:
-       </DialogTitle>
+        <Formik
+            initialValues={{
+                name: '',
+                description: '',
+                services: []
+            }}
+            validationSchema={DisplayingErrorMessagesPackageSchema}
+            validateOnChange
+            validateOnBlur
+            onSubmit={submitHandler}
+            onReset={handleReset}
+        >
+            {({ errors, handleBlur, handleChange, values, setFieldValue }) => {
 
-       <DialogContent dividers>
-        <Grid item>
-         <TextField
-          fullWidth
-          label="Tên gói dịch vụ"
-          defaultValue={data.name}
-          error={!!errors.name}
-          helperText={errors.name}
-          margin="normal"
-          name="name"
-          variant="outlined"
-          onBlur={handleBlur}
-          onChange={handleChange}
-         />
-        </Grid>
-        <Grid item>
-         <TextField
-          multiline
-          fullWidth
-          label="Mô tả"
-          margin="normal"
-          defaultValue={data.description}
-          error={!!errors.description}
-          helperText={errors.description}
-          onBlur={handleBlur}
-          onChange={handleChange}
-          name="description"
-          variant="outlined"
-         />
-        </Grid>
-        <Grid item>
-         <Autocomplete
-          multiple
-          id="tags-outlined"
-          options={services || []}
-          getOptionLabel={(option) => option.name}
-          defaultValue={data.services?.map((ser) => ser)}
-          onChange={(_, value) => {
-           setFieldValue('services update', value);
-           setCalculatedTotal(value);
-          }}
-          filterSelectedOptions
-          renderInput={(params) => (
-           <TextField
-            {...params}
-            variant="outlined"
-            label="Dịch vụ"
-            name="services"
-            error={!!errors.services}
-            helperText={errors.services}
-           />
-          )}
-         />
-        </Grid>
-        <Grid item>
-         <TextField
-          fullWidth
-          label="Giá tiền"
-          margin="normal"
-          error={!!errors.price}
-          helperText={errors.price}
-          onBlur={handleBlur}
-          onChange={handleChange}
-          defaultValue={data.price}
-          value={values.price}
-          name="price"
-          variant="outlined"
-          InputProps={{
-           readOnly: true
-          }}
-         />
-        </Grid>
-       </DialogContent>
-       <DialogActions>
-        <Button type="submit" color="primary" left="true">
-         Lưu
-        </Button>
-        <Button onClick={onClose} type="reset" color="secondary">
-         Hủy
-        </Button>
-       </DialogActions>
-      </Form>
-     </Dialog>
+                useEffect(() => {
+                    setFieldValue('price', totalPrice);
+                }, [totalPrice, setFieldValue]);
+
+                return (
+                    <Dialog
+                        onClose={onClose}
+                        aria-describedby="scroll-dialog-description"
+                        open={open}
+                        maxWidth={'xs'}
+                        fullWidth={true}
+                    >
+                        <Form>
+                            <DialogTitle id="customized-dialog-title" onClose={onClose}>
+                                Gói Dịch vụ:
+                            </DialogTitle>
+
+                            <DialogContent dividers>
+                                <Grid item>
+                                    <TextField
+                                        fullWidth
+                                        label="Tên gói dịch vụ"
+                                        defaultValue={data.name}
+                                        error={!!errors.name}
+                                        helperText={errors.name}
+                                        margin="normal"
+                                        name="name"
+                                        variant="outlined"
+                                        onBlur={handleBlur}
+                                        onChange={handleChange}
+                                    />
+                                </Grid>
+                                <Grid item>
+                                    <TextField
+                                        multiline
+                                        fullWidth
+                                        label="Mô tả"
+                                        margin="normal"
+                                        defaultValue={data.description}
+                                        error={!!errors.description}
+                                        helperText={errors.description}
+                                        onBlur={handleBlur}
+                                        onChange={handleChange}
+                                        name="description"
+                                        variant="outlined"
+                                    />
+                                </Grid>
+                                <Grid item>
+                                    <Autocomplete
+                                        multiple
+                                        id="tags-outlined"
+                                        options={services || []}
+                                        getOptionLabel={(option) => option.name}
+                                        defaultValue={data.services?.map((ser) => ser)}
+                                        onChange={(_, value) => {
+                                            setFieldValue('services update', value);
+                                            setCalculatedTotal(value);
+                                        }}
+                                        filterSelectedOptions
+                                        renderInput={(params) => (
+                                            <TextField
+                                                {...params}
+                                                variant="outlined"
+                                                label="Dịch vụ"
+                                                name="services"
+                                                error={!!errors.services}
+                                                helperText={errors.services}
+                                            />
+                                        )}
+                                    />
+                                </Grid>
+                                <Grid item>
+                                    <TextField
+                                        fullWidth
+                                        label="Giá tiền"
+                                        margin="normal"
+                                        error={!!errors.price}
+                                        helperText={errors.price}
+                                        onBlur={handleBlur}
+                                        onChange={handleChange}
+                                        defaultValue={data.price || values.price}
+                                        value={values.price}
+                                        name="price"
+                                        variant="outlined"
+                                        InputProps={{
+                                            readOnly: true
+                                        }}
+                                    />
+                                </Grid>
+                            </DialogContent>
+                            <DialogActions>
+                                <Button type="submit" color="primary" left="true">
+                                    Lưu
+                                </Button>
+                                <Button onClick={onClose} type="reset" color="secondary">
+                                    Hủy
+                                </Button>
+                            </DialogActions>
+                        </Form>
+                    </Dialog>
+                );
+            }}
+        </Formik>
     );
-   }}
-  </Formik>
- );
 };
 export default memo(DialogUpdatePackage);
