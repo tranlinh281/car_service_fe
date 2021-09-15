6dfc57d35225d17511f24100cdee3eca49794126
TranLinh <tranlinhhqb@gmail.com>
2021-09-15 16:35:09 +0700

    LinhTT - Update absences work of staff

--- src/actions/userAction.js
+++ src/actions/userAction.js
@@ -1,192 +1,196 @@
+import { List } from '@material-ui/core';
 import Axios from 'axios';
 import {
- CREATE_EMPLOYEE_FAIL,
- CREATE_EMPLOYEE_REQUEST,
- CREATE_EMPLOYEE_SUCCESS,
- EMPLOYEE_LIST_FAIL,
- EMPLOYEE_LIST_REQUEST,
- EMPLOYEE_LIST_SUCCESS,
- USER_LOGIN_FAIL,
- USER_LOGIN_REQUEST,
- USER_LOGIN_SUCCESS,
- USER_LOGOUT,
- TRIGGER_RELOAD,
- DELETE_EMPLOYEE_REQUEST,
- DELETE_EMPLOYEE_SUCCESS,
- DELETE_EMPLOYEE_FAIL,
- EDIT_EMPLOYEE_REQUEST,
- EDIT_EMPLOYEE_SUCCESS,
- EDIT_EMPLOYEE_FAIL,
- EMPLOYEE_ABSENT_LIST_REQUEST,
- EMPLOYEE_ABSENT_LIST_FAIL,
- EMPLOYEE_ABSENT_LIST_SUCCESS,
- EDIT_EMPLOYEE_ABSENT_SUCCESS,
- EDIT_EMPLOYEE_ABSENT_REQUEST,
- EDIT_EMPLOYEE_ABSENT_FAIL
+  CREATE_EMPLOYEE_FAIL,
+  CREATE_EMPLOYEE_REQUEST,
+  CREATE_EMPLOYEE_SUCCESS,
+  EMPLOYEE_LIST_FAIL,
+  EMPLOYEE_LIST_REQUEST,
+  EMPLOYEE_LIST_SUCCESS,
+  USER_LOGIN_FAIL,
+  USER_LOGIN_REQUEST,
+  USER_LOGIN_SUCCESS,
+  USER_LOGOUT,
+  TRIGGER_RELOAD,
+  DELETE_EMPLOYEE_REQUEST,
+  DELETE_EMPLOYEE_SUCCESS,
+  DELETE_EMPLOYEE_FAIL,
+  EDIT_EMPLOYEE_REQUEST,
+  EDIT_EMPLOYEE_SUCCESS,
+  EDIT_EMPLOYEE_FAIL,
+  EMPLOYEE_ABSENT_LIST_REQUEST,
+  EMPLOYEE_ABSENT_LIST_FAIL,
+  EMPLOYEE_ABSENT_LIST_SUCCESS,
+  EDIT_EMPLOYEE_ABSENT_SUCCESS,
+  EDIT_EMPLOYEE_ABSENT_REQUEST,
+  EDIT_EMPLOYEE_ABSENT_FAIL
 } from 'src/constants/userConstant';
 import {
- DELETE_EMPLOYEE,
- getEmployeeAbsentPagingURL,
- getEmployeePagingURL,
- GET_EMPLOYEE_BY_USERNAME_URL,
- GET_EMPLOYEE_LIST_URL,
- LOGIN_URL,
- POST_NEW_EMPLOYEE,
- UPDATE_ABSENT_EMPLOYEE_URL,
- UPDATE_EMPLOYEE_URL
+  DELETE_EMPLOYEE,
+  getEmployeeAbsentPagingURL,
+  getEmployeePagingURL,
+  GET_EMPLOYEE_BY_USERNAME_URL,
+  GET_EMPLOYEE_LIST_URL,
+  LOGIN_URL,
+  POST_NEW_EMPLOYEE,
+  UPDATE_ABSENT_EMPLOYEE_URL,
+  UPDATE_EMPLOYEE_URL
 } from 'src/services/Config';
 
 const headers = {
- 'Content-Type': 'application/json',
- 'Access-Control-Allow-Origin': '*',
- Accept: 'application/json, text/plain, */*'
- // Authorization:
- //   'Bearer ' +
- //   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiMTExMTEyIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy9yb2xlIjoiUXVhblRyaSIsIm5iZiI6MTYyMzA4MDI2MSwiZXhwIjoxNjIzMDgzODYxfQ.kJxGYbJzRjCCg4qy3OO0XjglTcuIOhoeY6ynmmxmwUo'
+  'Content-Type': 'application/json',
+  'Access-Control-Allow-Origin': '*',
+  Accept: 'application/json, text/plain, */*'
+  // Authorization:
+  //   'Bearer ' +
+  //   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiMTExMTEyIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy9yb2xlIjoiUXVhblRyaSIsIm5iZiI6MTYyMzA4MDI2MSwiZXhwIjoxNjIzMDgzODYxfQ.kJxGYbJzRjCCg4qy3OO0XjglTcuIOhoeY6ynmmxmwUo'
 };
 
 export const login = (username, password) => async (dispatch) => {
- dispatch({ type: USER_LOGIN_REQUEST, payload: { username, password } });
- try {
-  const { data } = await Axios.post(LOGIN_URL, { username, password });
-  dispatch({ type: USER_LOGIN_SUCCESS, payload: data });
-  localStorage.setItem('userInfo', JSON.stringify(data));
- } catch (error) {
-  const message =
-   error.respone && error.respone.data.message
-    ? error.respone.data.message
-    : error.message;
-  dispatch({ type: USER_LOGIN_FAIL, payload: message });
- }
+  dispatch({ type: USER_LOGIN_REQUEST, payload: { username, password } });
+  try {
+    const { data } = await Axios.post(LOGIN_URL, { username, password });
+    dispatch({ type: USER_LOGIN_SUCCESS, payload: data });
+    localStorage.setItem('userInfo', JSON.stringify(data));
+  } catch (error) {
+    const message =
+      error.respone && error.respone.data.message
+        ? error.respone.data.message
+        : error.message;
+    dispatch({ type: USER_LOGIN_FAIL, payload: message });
+  }
 };
 
 export const logout = () => (dispatch) => {
- localStorage.removeItem('userInfo');
- dispatch({ type: USER_LOGOUT });
+  localStorage.removeItem('userInfo');
+  dispatch({ type: USER_LOGOUT });
 };
 
 export const listEmployee = (keySearch, page) => async (dispatch) => {
- dispatch({ type: EMPLOYEE_LIST_REQUEST });
- try {
-  if (keySearch == undefined || keySearch == '') {
-   const { data } = await Axios.get(getEmployeePagingURL(page));
-   dispatch({ type: EMPLOYEE_LIST_SUCCESS, payload: data });
-   dispatch({ type: EMPLOYEE_LIST_FAIL, payload: '' });
-  } else {
-   const { data } = await Axios.get(getEmployeePagingURL(page) + keySearch);
-   dispatch({ type: EMPLOYEE_LIST_SUCCESS, payload: data });
+  dispatch({ type: EMPLOYEE_LIST_REQUEST });
+  try {
+    if (keySearch == undefined || keySearch == '') {
+      const { data } = await Axios.get(getEmployeePagingURL(page));
+      dispatch({ type: EMPLOYEE_LIST_SUCCESS, payload: data });
+      dispatch({ type: EMPLOYEE_LIST_FAIL, payload: '' });
+    } else {
+      const { data } = await Axios.get(getEmployeePagingURL(page) + keySearch);
+      dispatch({ type: EMPLOYEE_LIST_SUCCESS, payload: data });
+    }
+  } catch (error) {
+    const message = error.response.data;
+    dispatch({ type: EMPLOYEE_LIST_FAIL, payload: message });
   }
- } catch (error) {
-  const message = error.response.data;
-  dispatch({ type: EMPLOYEE_LIST_FAIL, payload: message });
- }
 };
 
 export const createEmployee = (employeeModels) => async (dispatch) => {
- dispatch({
-  type: CREATE_EMPLOYEE_REQUEST,
-  payload: { employeeModels }
- });
- try {
-  const { data } = await Axios.post(POST_NEW_EMPLOYEE, employeeModels);
-  dispatch({ type: CREATE_EMPLOYEE_SUCCESS, payload: data });
- } catch (error) {
   dispatch({
-   type: CREATE_EMPLOYEE_FAIL,
-   payload:
-    error.response && error.response.data.message
-     ? error.response.data.message
-     : error.message
+    type: CREATE_EMPLOYEE_REQUEST,
+    payload: { employeeModels }
   });
- }
+  try {
+    const { data } = await Axios.post(POST_NEW_EMPLOYEE, employeeModels);
+    dispatch({ type: CREATE_EMPLOYEE_SUCCESS, payload: data });
+  } catch (error) {
+    dispatch({
+      type: CREATE_EMPLOYEE_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message
+    });
+  }
 };
 
 export const triggerReload = () => async (dispatch) => {
- dispatch({
-  type: TRIGGER_RELOAD,
-  payload: {}
- });
+  dispatch({
+    type: TRIGGER_RELOAD,
+    payload: {}
+  });
 };
 
 export const deleteEmployee = (username) => async (dispatch) => {
- dispatch({
-  type: DELETE_EMPLOYEE_REQUEST,
-  payload: { username }
- });
- try {
-  const { data } = await Axios.delete(DELETE_EMPLOYEE + username, {
-   headers: headers
-  });
-  dispatch({ type: DELETE_EMPLOYEE_SUCCESS, payload: data });
-  console.log(data);
- } catch (error) {
   dispatch({
-   type: DELETE_EMPLOYEE_FAIL,
-   payload:
-    error.response && error.response.data.message
-     ? error.response.data.message
-     : error.message
+    type: DELETE_EMPLOYEE_REQUEST,
+    payload: { username }
   });
- }
+  try {
+    const { data } = await Axios.delete(DELETE_EMPLOYEE + username, {
+      headers: headers
+    });
+    dispatch({ type: DELETE_EMPLOYEE_SUCCESS, payload: data });
+    console.log(data);
+  } catch (error) {
+    dispatch({
+      type: DELETE_EMPLOYEE_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message
+    });
+  }
 };
 
 export const updateEmployee = (employeeModels) => async (dispatch) => {
- dispatch({
-  type: EDIT_EMPLOYEE_REQUEST,
-  payload: { employeeModels }
- });
+  dispatch({
+    type: EDIT_EMPLOYEE_REQUEST,
+    payload: { employeeModels }
+  });
 
- try {
-  const { data } = await Axios.put(UPDATE_EMPLOYEE_URL, employeeModels);
-  dispatch({ type: EDIT_EMPLOYEE_SUCCESS, payload: data });
-  dispatch(triggerReload({}));
- } catch (error) {
-  const message =
-   error.response && error.response.data.message
-    ? error.response.data.message
-    : error.message;
-  dispatch({ type: EDIT_EMPLOYEE_FAIL, payload: message });
- }
+  try {
+    const { data } = await Axios.put(UPDATE_EMPLOYEE_URL, employeeModels);
+    dispatch({ type: EDIT_EMPLOYEE_SUCCESS, payload: data });
+    dispatch(triggerReload({}));
+  } catch (error) {
+    const message =
+      error.response && error.response.data.message
+        ? error.response.data.message
+        : error.message;
+    dispatch({ type: EDIT_EMPLOYEE_FAIL, payload: message });
+  }
 };
 
 export const listEmployeeAbsent = (keySearch, page) => async (dispatch) => {
- dispatch({ type: EMPLOYEE_ABSENT_LIST_REQUEST });
- try {
-  if (keySearch == undefined || keySearch == '') {
-   const { data } = await Axios.get(getEmployeeAbsentPagingURL(page));
-   dispatch({ type: EMPLOYEE_ABSENT_LIST_SUCCESS, payload: data });
-   dispatch({ type: EMPLOYEE_ABSENT_LIST_FAIL, payload: '' });
-  } else {
-   const { data } = await Axios.get(
-    getEmployeeAbsentPagingURL(page) + keySearch
-   );
-   dispatch({ type: EMPLOYEE_ABSENT_LIST_FAIL, payload: data });
+  dispatch({ type: EMPLOYEE_ABSENT_LIST_REQUEST });
+  try {
+    if (keySearch == undefined || keySearch == '') {
+      const { data } = await Axios.get(getEmployeeAbsentPagingURL(page));
+      dispatch({ type: EMPLOYEE_ABSENT_LIST_SUCCESS, payload: data });
+      dispatch({ type: EMPLOYEE_ABSENT_LIST_FAIL, payload: '' });
+    } else {
+      const { data } = await Axios.get(
+        getEmployeeAbsentPagingURL(page) + keySearch
+      );
+      dispatch({ type: EMPLOYEE_ABSENT_LIST_FAIL, payload: data });
+    }
+  } catch (error) {
+    const message =
+      error.response && error.response.data.message
+        ? error.response.data.message
+        : error.message;
+    dispatch({ type: EMPLOYEE_ABSENT_LIST_FAIL, payload: message });
   }
- } catch (error) {
-  const message =
-   error.response && error.response.data.message
-    ? error.response.data.message
-    : error.message;
-  dispatch({ type: EMPLOYEE_ABSENT_LIST_FAIL, payload: message });
- }
 };
 
 export const updateAbsenceEmployee = (dataNew) => async (dispatch) => {
- console.log(dataNew, 'debug usser action');
- dispatch({
-  type: EDIT_EMPLOYEE_ABSENT_REQUEST,
-  payload: { dataNew }
- });
+  //  console.log(dataNew, 'debug usser action');
+  const arr = []
+  arr.push(dataNew)
+  dispatch({
+    type: EDIT_EMPLOYEE_ABSENT_REQUEST,
+    payload: { arr }
+  });
 
- try {
-  const { data } = await Axios.put(UPDATE_ABSENT_EMPLOYEE_URL, dataNew);
-  dispatch({ type: EDIT_EMPLOYEE_ABSENT_SUCCESS, payload: data });
-  dispatch(triggerReload({}));
- } catch (error) {
-  const message =
-   error.response && error.response.data.message
-    ? error.response.data.message
-    : error.message;
-  dispatch({ type: EDIT_EMPLOYEE_ABSENT_FAIL, payload: message });
- }
+  try {
+    const { data } = await Axios.put(UPDATE_ABSENT_EMPLOYEE_URL, arr);
+    console.log(data, 'debug usser action');
+    dispatch({ type: EDIT_EMPLOYEE_ABSENT_SUCCESS, payload: data });
+    dispatch(triggerReload({}));
+  } catch (error) {
+    const message =
+      error.response && error.response.data.message
+        ? error.response.data.message
+        : error.message;
+    dispatch({ type: EDIT_EMPLOYEE_ABSENT_FAIL, payload: message });
+  }
 };
--- src/pages/CalenderOfEmployee.js
+++ src/pages/CalenderOfEmployee.js
@@ -1,9 +1,9 @@
 import {
- Box,
- Card,
- CardContent,
- CardHeader,
- Container
+  Box,
+  Card,
+  CardContent,
+  CardHeader,
+  Container
 } from '@material-ui/core';
 import { Check, Close } from '@material-ui/icons';
 import moment from 'moment';
@@ -12,8 +12,8 @@ import { Calendar, momentLocalizer, Views } from 'react-big-calendar';
 import { Helmet } from 'react-helmet';
 import { useDispatch, useSelector } from 'react-redux';
 import {
- listEmployeeAbsent,
- updateAbsenceEmployee
+  listEmployeeAbsent,
+  updateAbsenceEmployee
 } from 'src/actions/userAction';
 import ButtonAction from 'src/components/ButtonAction';
 import ConfirmDialog from 'src/components/dialog/dialogConfirm';
@@ -24,149 +24,151 @@ const localizer = momentLocalizer(moment);
 let allViews = Object.keys(Views).map((k) => Views[k]);
 
 const ComponentTezt = ({ ...props }) => {
- const dispatch = useDispatch();
- const [confirmDialog, setConfirmDialog] = useState({
-  isOpen: false,
-  title: '',
-  subTitle: ''
- });
- const acceptAbsentEmployee = (events, isApproved) => {
-  const id = events.event.id;
-  const dataNew = {
-   id,
-   isApproved
+  const dispatch = useDispatch();
+  const [confirmDialog, setConfirmDialog] = useState({
+    isOpen: false,
+    title: '',
+    subTitle: ''
+  });
+  
+  const acceptAbsentEmployee = (events, isApproved) => {
+    const id = events.event.id;
+    const dataNew = {
+      id,
+      isApproved
+    };
+    dispatch(updateAbsenceEmployee(dataNew));
   };
-  dispatch(updateAbsenceEmployee(dataNew));
- };
- const dennyAbsentEmployee = (events, isApprove) => {
-  const id = events.event.id;
-  const dataNew = {
-   id,
-   isApproved
-  };
-  dispatch(updateAbsenceEmployee(dataNew));
- };
- const approved = props.event.isApproved;
 
- return (
-  <>
-   <Box sx={{ display: 'flex', justifyContent: 'flex-start' }}>
-    {props.title}, {props.event.noteEmp}
-   </Box>{' '}
-   <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
-    {approved === null ? (
-     <Box>
-      <ButtonAction
-       color="secondary"
-       onClick={() => {
-        setConfirmDialog({
-         isOpen: true,
-         title: 'Bạn có chắc muốn duyệt ngày nghỉ này?',
-         onConfirm: () => {
-          acceptAbsentEmployee(props, true),
-           setConfirmDialog({ isOpen: false });
-         }
-        });
-       }}
-      >
-       <Check fontSize="small" color="primary" />
-      </ButtonAction>
-      <ButtonAction
-       color="secondary"
-       onClick={() => {
-        setConfirmDialog({
-         isOpen: true,
-         title: 'Bạn có chắc muốn từ chối ngày nghỉ của nhân viên?',
-         onConfirm: () => {
-          dennyAbsentEmployee(props, false),
-           setConfirmDialog({ isOpen: false });
-         }
-        });
-       }}
-      >
-       <Close fontSize="small" color="secondary" />
-      </ButtonAction>
-     </Box>
-    ) : (
-     <div>Đã duyệt</div>
-    )}{' '}
-   </Box>
-   <ConfirmDialog
-    confirmDialog={confirmDialog}
-    setConfirmDialog={setConfirmDialog}
-   />
-  </>
- );
+  const dennyAbsentEmployee = (events, isApprove) => {
+    const id = events.event.id;
+    const dataNew = {
+      id,
+      isApprove
+    };
+    dispatch(updateAbsenceEmployee(dataNew));
+  };
+  const approved = props.event.isApproved;
+
+  return (
+    <>
+      <Box sx={{ display: 'flex', justifyContent: 'flex-start' }}>
+        {props.title}, {props.event.noteEmp}
+      </Box>{' '}
+      <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+        {approved === null ? (
+          <Box>
+            <ButtonAction
+              color="secondary"
+              onClick={() => {
+                setConfirmDialog({
+                  isOpen: true,
+                  title: 'Bạn có chắc muốn duyệt ngày nghỉ này?',
+                  onConfirm: () => {
+                    acceptAbsentEmployee(props, true),
+                      setConfirmDialog({ isOpen: false });
+                  }
+                });
+              }}
+            >
+              <Check fontSize="small" color="primary" />
+            </ButtonAction>
+            <ButtonAction
+              color="secondary"
+              onClick={() => {
+                setConfirmDialog({
+                  isOpen: true,
+                  title: 'Bạn có chắc muốn từ chối ngày nghỉ của nhân viên?',
+                  onConfirm: () => {
+                    dennyAbsentEmployee(props, false),
+                      setConfirmDialog({ isOpen: false });
+                  }
+                });
+              }}
+            >
+              <Close fontSize="small" color="secondary" />
+            </ButtonAction>
+          </Box>
+        ) : (
+          <div>Đã duyệt</div>
+        )}{' '}
+      </Box>
+      <ConfirmDialog
+        confirmDialog={confirmDialog}
+        setConfirmDialog={setConfirmDialog}
+      />
+    </>
+  );
 };
 
 const CalenderOfEmployee = () => {
- const { data, error, loading } = useSelector(
-  (state) => state.employeeAbsentList
- );
- const dispatch = useDispatch();
- const dataItem = data.itemsList;
- console.log(dataItem, 'debug itemslist');
+  const { data, error, loading } = useSelector(
+    (state) => state.employeeAbsentList
+  );
+  const dispatch = useDispatch();
+  const dataItem = data.itemsList;
+  console.log(dataItem, 'debug itemslist');
 
- let eventList = [];
+  let eventList = [];
 
- if (dataItem && dataItem.length) {
-  dataItem.map((item) => {
-   const absentListOfUser = item.absences.map(
-    ({ timeEnd, timeStart, id, isApproved, noteEmp }) => ({
-     id,
-     title: item.fullname,
-     start: new Date(timeStart),
-     end: new Date(timeEnd),
-     noteEmp,
-     isApproved,
-     allDay: true,
-     status: 'asdhjkasdjkahsd'
-    })
-   );
+  if (dataItem && dataItem.length) {
+    dataItem.map((item) => {
+      const absentListOfUser = item.absences.map(
+        ({ timeEnd, timeStart, id, isApproved, noteEmp }) => ({
+          id,
+          title: item.fullname,
+          start: new Date(timeStart),
+          end: new Date(timeEnd),
+          noteEmp,
+          isApproved,
+          allDay: true,
+          status: 'asdhjkasdjkahsd'
+        })
+      );
 
-   eventList = [...eventList, ...absentListOfUser];
-  });
- }
+      eventList = [...eventList, ...absentListOfUser];
+    });
+  }
 
- console.log('debug', eventList);
+  console.log('debug', eventList);
 
- useEffect(() => {
-  dispatch(listEmployeeAbsent());
- }, [dispatch]);
+  useEffect(() => {
+    dispatch(listEmployeeAbsent());
+  }, [dispatch]);
 
- return (
-  <AccessoryDialogHOC>
-   <Helmet>
-    <title>{constant.CALENDER_TITLE}</title>
-   </Helmet>
-   <Box
-    sx={{
-     minHeight: '100%',
-     py: 3
-    }}
-   >
-    <Container maxWidth={false}>
-     <Card>
-      <CardHeader title="Lịch nghỉ của nhân viên" />
-      <CardContent>
-       <Calendar
-        components={{
-         agenda: {
-          event: ComponentTezt
-         }
+  return (
+    <AccessoryDialogHOC>
+      <Helmet>
+        <title>{constant.CALENDER_TITLE}</title>
+      </Helmet>
+      <Box
+        sx={{
+          minHeight: '100%',
+          py: 3
         }}
-        events={eventList}
-        showMultiDayTimes={false}
-        localizer={localizer}
-        views={['week', 'agenda']}
-        defaultView={'week'}
-       />
-      </CardContent>
-     </Card>
-    </Container>
-   </Box>
-  </AccessoryDialogHOC>
- );
+      >
+        <Container maxWidth={false}>
+          <Card>
+            <CardHeader title="Lịch nghỉ của nhân viên" />
+            <CardContent>
+              <Calendar
+                components={{
+                  agenda: {
+                    event: ComponentTezt
+                  }
+                }}
+                events={eventList}
+                showMultiDayTimes={false}
+                localizer={localizer}
+                views={['week', 'agenda']}
+                defaultView={'week'}
+              />
+            </CardContent>
+          </Card>
+        </Container>
+      </Box>
+    </AccessoryDialogHOC>
+  );
 };
 
 export default CalenderOfEmployee;
